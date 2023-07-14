fe57aac06a791e5d4b15ee2857f1b925a8dd459b
Aliceeeee2023 <136624667+Aliceeeee2023@users.noreply.github.com>
2023-07-14 19:28:11 +0800

    Update Week2.py

--- Week2/Week2.py
+++ Week2/Week2.py
@@ -121,7 +121,7 @@ def func(*data):
         for detailedlocation in location:
             print(data[detailedlocation])
 
-func("彭⼤牆", "王明雅", "吳安") # print 彭⼤牆
+func("彭⼤牆", "王明雅", "吳明") # print 彭⼤牆
 func("郭靜雅", "王立強", "林靜宜", "郭立恆", "林花花") # print 林花花
 func("郭宣雅", "林靜宜", "郭宣恆", "林靜花") # print 沒有
 
